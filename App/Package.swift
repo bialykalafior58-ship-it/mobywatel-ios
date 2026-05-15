3ae597154facbcba12d6f27b2d1fda0f9e66a08f
igus <rambriepl123@gmail.com>
2026-05-16 01:27:03 +0200

    Update Package.swift

--- App/Package.swift
+++ App/Package.swift
@@ -1,8 +1,33 @@
+// swift-tools-version: 6.0
 
 import PackageDescription
 
 let package = Package(
-  name: "app", 
-  products: [], 
-  targets: [] 
-)
+    name: "App",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .iOSApplication(
+            name: "Mobywatel",
+            executableName: "Mobywatel",
+            bundleIdentifier: "com.bialykalafior.mobywatel",
+            teamIdentifier: "",
+            displayVersion: "1.0",
+            bundleVersion: "1",
+            appIcon: .asset("AppIcon"),
+            accentColor: .asset("AccentColor"),
+            supportedDeviceFamilies: [.pad, .phone],
+            supportedInterfaceOrientations: [.portrait, .portraitUpsideDown, .landscapeLeft, .landscapeRight],
+            capabilities: [],
+            additionalInfoPlistContentFile: "Info.plist"
+        )
+    ],
+    targets: [
+        .executableTarget(
+            name: "App",
+            dependencies: [],
+            path: "Sources"
+        )
+    ]
+)	
